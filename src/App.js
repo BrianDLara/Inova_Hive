72d42a4bb0e392d298d70770a9a695a827acf0c3
BrianDLara <96392130+BrianDLara@users.noreply.github.com>
2023-05-05 22:42:37 -0400

    updated the navbar and dark mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-import Nav from './components/Nav'
+import Nav from './components/Nav';
 const App = () => {
+  const [mode, setMode] = useState('light'); // default mode is light
+
+  const toggleMode = () => {
+    setMode(mode === 'light' ? 'dark' : 'light');
+  };
   return (
-    <div className="App">
+    <div className={mode === 'dark' ? 'bg-gray-900 text-white' : ''}>
       <header>
-        <Nav />
+        <Nav mode={mode} toggleMode={toggleMode} />
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-const Nav = () => {
+const Nav = ({ mode, toggleMode }) => {
     const logo = 'https://i.imgur.com/8lqGFxZ.png'; // replace with your Imgur link
 
     return (
-        <header className="bg-gray-900">
+        <header className={`flex justify-between items-center p-4 ${mode === 'dark' ? 'bg-gray-800' : 'bg-gray-100'}`}>
       <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
@@ -19,6 +19,9 @@ const Nav = () => {
               <li><a href="#" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact Us</a></li>
             </ul>
           </div>
+          <button className={`px-4 py-2 rounded ${mode === 'dark' ? 'bg-gray-600 text-white' : 'bg-gray-200 text-gray-800'}`} onClick={toggleMode}>
+        {mode === 'dark' ? 'Light Mode' : 'Dark Mode'}
+      </button>
           <div className="-mr-2 flex md:hidden">
             <button type="button" className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
               <span className="sr-only">Open main menu</span>
